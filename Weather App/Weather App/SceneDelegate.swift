8d5cd3d0ecc4d71f96ee0323f35df9335b25f665
Milena Hikari <milenahikari@MacBook-Air-de-Milena.local>
2023-10-31 21:27:00 -0300

    feat: removing storyboard settings

--- Weather App/Weather App/SceneDelegate.swift
+++ Weather App/Weather App/SceneDelegate.swift
@@ -16,7 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Frame é a definição de tamanho - a window irá ter o tamanhao de coordenadas da windowScene
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        
+        // Instancia da ViewControler
+        window?.rootViewController = ViewController()
+        
+        // Torna a window visível
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
