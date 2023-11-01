b257f137acc123aad4cf20a131fd74dce5a0f8db
Milena Hikari <milenahikari@MacBook-Air-de-Milena.local>
2023-11-01 08:20:47 -0300

    feat: learning about constraints and auto layout

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        // É executado sempre que a ViewController é carregada e apenas 1 vez
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .red
+        
+        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        
+        // Atalho para utilizar a cor do UIColor
+        customView.backgroundColor = .black
+        
+        view.addSubview(customView)
     }
 
 
